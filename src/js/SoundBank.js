1022221996c912d75832fe62ba62f7776fa187c9
MikeGajardo <mikegajardo@gmail.com>
2022-09-09 22:01:22 +0000

    updated logic

---
+++ src/js/SoundBank.js
@@ -0,0 +1,42 @@
+export const SoundBank = [
+    {
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+        description: " "
+    },
+    {
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+        description: "Heater 1"
+    },
+    {
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+        description: "Heater 2"
+    },
+    {
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+        description: "Heater 3"
+    },
+    {
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+        description: "Heater 4"
+    },
+    {
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+        description: "Clap"
+    },
+    {
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+        description: "Open Hi Hat"
+    },
+    {
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+        description: "Kick-n Hat"
+    },
+    {
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+        description: "Kick"
+    },
+    {
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+        description: "Closed Hi-hat"
+    },
+]
--- src/js/component/DrumMachine.jsx
+++ src/js/component/DrumMachine.jsx
@@ -1,29 +1,54 @@
 import React from "react";
 import { useState } from "react";
+import { SoundBank } from "../SoundBank";
+
+// import heater1 from "../../img/heater1.mp3";
+// import heater2 from "../../img/heater2.mp3";
+// import heater3 from "../../img/heater3.mp3";
+// import heater4 from "../../img/heater4.mp3";
+// import heater6 from "../../img/heater6.mp3";
+// import hihatclosed from "../../img/hihatclosed.mp3";
+// import hihatopen from "../../img/hihatopen.mp3";
+// import kick1 from "../../img/kick1.mp3";
+// import kicknhat from "../../img/kicknhat.mp3";
+
 
 export const DrumMachine = () => {
+  const [currSound, setCurrSound] = useState(0)
+
+
   return (
     <div id="drum-machine">
-        <div id="display">
+      <div id="display"><h3>{SoundBank[currSound].description}</h3></div>
+        <div className="machine">
           <div className="container">
             <div className="row">
-              <div className="col-sm drum-pad" id="Q"></div>
-              <div className="col-sm drum-pad" id="W"></div>
-              <div className="col-sm drum-pad" id="E"></div>
-          </div>
-          </div>
-          <div className="container">
-            <div className="row">
-            <div className="col-sm drum-pad" id="A"></div>
-            <div className="col-sm drum-pad" id="S"></div>
-            <div className="col-sm drum-pad" id="D"></div>
+              <div className="col-sm drum-pad" id="Q" onClick={() =>
+              setCurrSound(1)}></div>
+              <div className="col-sm drum-pad" id="W" onClick={() =>
+              setCurrSound(2)}></div>
+              <div className="col-sm drum-pad" id="E" onClick={() =>
+              setCurrSound(3)}></div>
           </div>
           </div>
           <div className="container">
             <div className="row">
-              <div className="col-sm drum-pad" id="Z"></div>
-              <div className="col-sm drum-pad" id="X"></div>
-              <div className="col-sm drum-pad" id="C"></div>
+            <div className="col-sm drum-pad" id="A" onClick={() =>
+              setCurrSound(4)}></div>
+            <div className="col-sm drum-pad" id="S" onClick={() =>
+              setCurrSound(5)}></div>
+            <div className="col-sm drum-pad" id="D" onClick={() =>
+              setCurrSound(6)}></div>
+          </div>
+          </div>
+          <div className="container">
+            <div className="row">
+              <div className="col-sm drum-pad" id="Z" onClick={() =>
+              setCurrSound(7)}></div>
+              <div className="col-sm drum-pad" id="X" onClick={() =>
+              setCurrSound(8)}></div>
+              <div className="col-sm drum-pad" id="C" onClick={() =>
+              setCurrSound(9)}></div>
             </div>
           </div> 
         </div>
